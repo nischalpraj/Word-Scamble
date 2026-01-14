89c51d914355677ae143b0e1ecde69732350541d
nischalpraj <nischalpraj776@gmail.com>
2026-01-14 15:55:45 +0545

    splitted each letter

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-const initgame =() => {
-    console.log("Game initialized");
-}
+const initgame = () => {
+  let randomObj = words[Math.floor(Math.random() * words.length)]; // getting random object from words
+  let wordsArray = randomObj.word.split(""); // splitting each letter of word
+  console.log(wordsArray);
+};
+initgame();
